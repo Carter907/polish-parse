1d9caf6650736113b2d802dba2f572764226826a
Carter S. <speers.carter@gmail.com>
2026-04-07 13:47:21 -0400

    refactor: fixed operands in polish.c

--- src/main.c
+++ src/main.c
@@ -27,7 +27,7 @@ void interpret() {
     ssize_t rt = read_line(&ln);
 
     if (rt == -1) {
-      perror("readline()");
+      perror("readline");
       exit(EXIT_FAILURE);
     }
 
--- src/polish.c
+++ src/polish.c
@@ -7,65 +7,64 @@
 #include "stack.h"
 
 typedef struct operands_t {
-  int op1;
-  int op2;
+  int first; // second element off the stack
+  int second; // first element off the stack
 
-} operands_t;
+} ops_t;
 
-operands_t get_operands(stack_t *stk_ptr) {
+ops_t get_operands(stack_t *stk_ptr) {
 
   int op_2 = stack_pop(stk_ptr);
   int op_1 = stack_pop(stk_ptr);
 
-  return (operands_t){.op1 = op_1, .op2 = op_2};
+  return (ops_t){ .first = op_1, .second = op_2 };
 }
 
 int parse_rev_polish(char *input, size_t in_size) {
 
   stack_t *stk = stack_create();
 
+	ops_t ops = {0};
+
   for (size_t i = 0; i < in_size; i++) {
-    int op1, op2;
+
+
     switch (input[i]) {
+
     case '+':
 
-      op2 = stack_pop(stk);
-      op1 = stack_pop(stk);
+			ops = get_operands(stk);
 
-      stack_push(stk, op1 + op2);
+      stack_push(stk, ops.first + ops.second);
 
       break;
 
     case '-':
 
-      op2 = stack_pop(stk);
-      op1 = stack_pop(stk);
+			ops = get_operands(stk);
 
-      stack_push(stk, op1 - op2);
+      stack_push(stk, ops.first - ops.second);
 
       break;
     case '*':
 
-      op2 = stack_pop(stk);
-      op1 = stack_pop(stk);
+			ops = get_operands(stk);
 
-      stack_push(stk, op1 * op2);
+      stack_push(stk, ops.first * ops.second);
 
       break;
     case '/':
 
-      op2 = stack_pop(stk);
-      op1 = stack_pop(stk);
+			ops = get_operands(stk);
 
-      stack_push(stk, op1 / op2);
+      stack_push(stk, ops.first / ops.second);
 
       break;
     case '^':
 
-      op2 = stack_pop(stk);
-      op1 = stack_pop(stk);
+			ops = get_operands(stk);
 
-      stack_push(stk, (int)pow(op1, op2));
+      stack_push(stk, (int)pow(ops.first, ops.second));
 
       break;
     case ' ':
@@ -86,9 +85,10 @@ int parse_rev_polish(char *input, size_t in_size) {
         fprintf(stderr, "failure to parse token\n");
         exit(EXIT_FAILURE);
       }
+
       // must be a number
 
-      // parse number that could be multiple digits long
+      // parse number 0 or more digits long (Kleene closure of digits)
 
       int j = (int)i + 1;
       int num = 0;
@@ -117,6 +117,7 @@ int parse_rev_polish(char *input, size_t in_size) {
       break;
     };
   }
+
   if (stk->top != 1) {
     stack_destroy(stk);
     fprintf(stderr, "invalid end state, check for valid notation\n");
